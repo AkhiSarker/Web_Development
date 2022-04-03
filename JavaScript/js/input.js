285f4557e9a8f9fe9fccf2ffa6ae50e9a115e4e7
Akhi Sarker <akhisarker9@gmail,com>
2022-04-03 20:04:40 +0600

    First commit

---
+++ JavaScript/js/input.js
@@ -0,0 +1,17 @@
+var x;
+x = prompt("Enter your name : ");
+console.log(x);
+document.write(x);
+
+var a
+a = 45;
+console.log(a);
+a = 35.33
+console.log(a);
+a = "AKHI SARKER";
+console.log(a);
+
+//let b = "Akhi";
+//console.log(b);
+
+const A = 2;
---
+++ JavaScript/js/script.js
@@ -0,0 +1,6 @@
+window.alert("I am learning Javascript");
+document.write("I am learning Javascript");
+document.getElementById("root").innerHTML = "I love javascript!";
+document.getElementById("idH1").innerHTML = "I am H1!";
+document.getElementById("idH3").innerHTML = "I am H3!";
+console.log("I am Bohubrihi!");
